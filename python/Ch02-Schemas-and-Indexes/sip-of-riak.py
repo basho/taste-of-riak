62b447c24dabe11c4dbaeab40c68cd7235447f19
Alex Moore <moore.alex@gmail.com>
2013-11-13 15:17:47 -0500

    Fixing Python Styling (PEP8)

--- python/Ch02-Schemas-and-Indexes/sip-of-riak.py
+++ python/Ch02-Schemas-and-Indexes/sip-of-riak.py
@@ -15,58 +15,58 @@ customer = {
 }
 
 orders = [
-  {
-      'order_id': 1,
-      'customer_id': 1,
-      'salesperson_id': 9000,
-      'items': [
-          {
-            'item_id': "TCV37GIT4NJ",
-            'title': "USB 3.0 Coffee Warmer",
-            'price': 15.99
-          },
-          {
-            'item_id': "PEG10BBF2PP",
-            'title': "eTablet Pro, 24GB, Grey",
-            'price': 399.99
-          }
-      ],
-      'total': 415.98,
-      'order_date': "2013-10-1 14:42:26"
-  },
-  {
-      'order_id': 2,
-      'customer_id': 1,
-      'salesperson_id': 9001,
-      'items': [
-          {
-            'item_id': "OAX19XWN0QP",
-            'title': "GoSlo Digital Camera",
-            'price': 359.99
-          }
-      ],
-      'total': 359.99,
-      'order_date': "2013-10-15 16:43:16"
-  },
-  {
-      'order_id': 3,
-      'customer_id': 1,
-      'salesperson_id': 9000,
-      'items': [
-          {
-            'item_id': "WYK12EPU5EZ",
-            'title': "Call of Battle: Goats - Gamesphere 4",
-            'price': 69.99
-          },
-          {
-            'item_id': "TJB84HAA8OA",
-            'title': "Bricko Building Blocks",
-            'price': 4.99
-          }
-      ],
-      'total': 74.98,
-      'order_date': "2013-11-3 17:45:28"
-  }]
+    {
+        'order_id': 1,
+        'customer_id': 1,
+        'salesperson_id': 9000,
+        'items': [
+            {
+                'item_id': "TCV37GIT4NJ",
+                'title': "USB 3.0 Coffee Warmer",
+                'price': 15.99
+            },
+            {
+                'item_id': "PEG10BBF2PP",
+                'title': "eTablet Pro, 24GB, Grey",
+                'price': 399.99
+            }
+        ],
+        'total': 415.98,
+        'order_date': "2013-10-1 14:42:26"
+    },
+    {
+        'order_id': 2,
+        'customer_id': 1,
+        'salesperson_id': 9001,
+        'items': [
+            {
+                'item_id': "OAX19XWN0QP",
+                'title': "GoSlo Digital Camera",
+                'price': 359.99
+            }
+        ],
+        'total': 359.99,
+        'order_date': "2013-10-15 16:43:16"
+    },
+    {
+        'order_id': 3,
+        'customer_id': 1,
+        'salesperson_id': 9000,
+        'items': [
+            {
+                'item_id': "WYK12EPU5EZ",
+                'title': "Call of Battle: Goats - Gamesphere 4",
+                'price': 69.99
+            },
+            {
+                'item_id': "TJB84HAA8OA",
+                'title': "Bricko Building Blocks",
+                'price': 4.99
+            }
+        ],
+        'total': 74.98,
+        'order_date': "2013-11-3 17:45:28"
+    }]
 
 order_summary = {
     'customer_id': 1,
@@ -101,14 +101,17 @@ order_summary_bucket = client.bucket('OrderSummaries')
 
 
 # Storing Data
-cr = customer_bucket.new(str(customer['customer_id']), data=customer)
+cr = customer_bucket.new(str(customer['customer_id']),
+                         data=customer)
 cr.store()
 
 for order in orders:
-  order_riak = order_bucket.new(str(order['order_id']), data=order)
-  order_riak.store()
+    order_riak = order_bucket.new(str(order['order_id']),
+                                  data=order)
+    order_riak.store()
 
-os = order_summary_bucket.new(str(order_summary['customer_id']), data=order_summary)
+os = order_summary_bucket.new(str(order_summary['customer_id']),
+                              data=order_summary)
 os.store()
 
 
@@ -121,12 +124,12 @@ customer
 
 # Adding Index Data
 
-for i in range(1,4):
-  order = order_bucket.get(str(i))
-  # Initialize our secondary indices
-  order.add_index('salesperson_id_int', order.data['salesperson_id'])
-  order.add_index('order_date_bin', order.data['order_date'])
-  order.store()
+for i in range(1, 4):
+    order = order_bucket.get(str(i))
+    # Initialize our secondary indices
+    order.add_index('salesperson_id_int', order.data['salesperson_id'])
+    order.add_index('order_date_bin', order.data['order_date'])
+    order.store()
 
 
 # Index Queries
@@ -135,6 +138,8 @@ for i in range(1,4):
 janes_orders = order_bucket.get_index("salesperson_id_int", 9000)
 janes_orders.results
 
-# Query for orders where the order_date_bin index is between 2013-10-01 and 2013-10-31
-october_orders = order_bucket.get_index("order_date_bin", "2013-10-01", "2013-10-31")
+# Query for orders where the order_date_bin index
+# is between 2013-10-01 and 2013-10-31
+october_orders = order_bucket.get_index("order_date_bin",
+                                        "2013-10-01", "2013-10-31")
 october_orders.results
