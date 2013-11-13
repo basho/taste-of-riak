8d98344416fcf29ff9d97a709a70dd63662e1783
Alex Moore <moore.alex@gmail.com>
2013-11-13 16:47:39 -0500

    Fixing PHP Style Checker Errors, excluding missing doc errors.

--- php/Ch02-Schemas-and-Indexes/sip-of-riak.php
+++ php/Ch02-Schemas-and-Indexes/sip-of-riak.php
@@ -1,17 +1,19 @@
 <?php 
-require_once('riak-php-client/src/Basho/Riak/Riak.php');
-require_once('riak-php-client/src/Basho/Riak/Bucket.php');
-require_once('riak-php-client/src/Basho/Riak/Exception.php');
-require_once('riak-php-client/src/Basho/Riak/Link.php');
-require_once('riak-php-client/src/Basho/Riak/MapReduce.php');
-require_once('riak-php-client/src/Basho/Riak/Object.php');
-require_once('riak-php-client/src/Basho/Riak/StringIO.php');
-require_once('riak-php-client/src/Basho/Riak/Utils.php');
-require_once('riak-php-client/src/Basho/Riak/Link/Phase.php');
-require_once('riak-php-client/src/Basho/Riak/MapReduce/Phase.php');
+require_once 'riak-php-client/src/Basho/Riak/Riak.php';
+require_once 'riak-php-client/src/Basho/Riak/Bucket.php';
+require_once 'riak-php-client/src/Basho/Riak/Exception.php';
+require_once 'riak-php-client/src/Basho/Riak/Link.php';
+require_once 'riak-php-client/src/Basho/Riak/MapReduce.php';
+require_once 'riak-php-client/src/Basho/Riak/Object.php';
+require_once 'riak-php-client/src/Basho/Riak/StringIO.php';
+require_once 'riak-php-client/src/Basho/Riak/Utils.php';
+require_once 'riak-php-client/src/Basho/Riak/Link/Phase.php';
+require_once 'riak-php-client/src/Basho/Riak/MapReduce/Phase.php';
 
 // Class definitions for our models
-class Customer {
+
+class Customer
+{
     var $customerId;
     var $name;
     var $address;
@@ -22,8 +24,10 @@ class Customer {
     var $createdDate;
 }
 
-class Order {
-    public function __construct() {
+class Order
+{
+    public function __construct()
+    {
         $this->items = array();
     }
     var $orderId;
@@ -34,8 +38,10 @@ class Order {
     var $orderDate;
 }
 
-class Item {
-    public function __construct($itemId, $title, $price) {
+class Item
+{
+    public function __construct($itemId, $title, $price)
+    {
         $this->itemId = $itemId;
         $this->title = $title;
         $this->price = $price;
@@ -45,16 +51,20 @@ class Item {
     var $price;
 }
 
-class OrderSummary {
-    public function __construct() {
+class OrderSummary
+{
+    public function __construct() 
+    {
         $this->summaries = array();
     }
     var $customerId;
     var $summaries;
 }
 
-class OrderSummaryItem {
-    public function __construct(Order $order) {
+class OrderSummaryItems
+{
+    public function __construct(Order $order) 
+    {
         $this->orderId = $order->orderId;
         $this->total = $order->total;
         $this->orderDate = $order->orderDate;
@@ -84,12 +94,17 @@ $order1->orderId = 1;
 $order1->customerId = 1;
 $order1->salespersonId = 9000;
 $order1->items = array(
-        new Item('TCV37GIT4NJ',
-                'USB 3.0 Coffee Warmer',
-                15.99),
-        new Item('PEG10BBF2PP', 
-                 'eTablet Pro; 24GB; Grey', 
-                 399.99));
+    new Item(
+        'TCV37GIT4NJ',
+        'USB 3.0 Coffee Warmer',
+        15.99
+    ),
+    new Item(
+        'PEG10BBF2PP', 
+        'eTablet Pro; 24GB; Grey', 
+        399.99
+    )
+);
 $order1->total = 415.98;
 $order1->orderDate = '2013-10-01 14:42:26';
 $orders[] = $order1;
@@ -99,9 +114,12 @@ $order2->orderId = 2;
 $order2->customerId = 1;
 $order2->salespersonId = 9001;
 $order2->items = array(
-        new Item('OAX19XWN0QP',
-                'GoSlo Digital Camera',
-                359.99));
+    new Item(
+        'OAX19XWN0QP',
+        'GoSlo Digital Camera',
+        359.99
+    )
+);
 $order2->total = 359.99;
 $order2->orderDate = '2013-10-15 16:43:16';
 $orders[] = $order2;
@@ -111,12 +129,17 @@ $order3->orderId = 3;
 $order3->customerId = 1;
 $order3->salespersonId = 9000;
 $order3->items = array(
-        new Item('WYK12EPU5EZ',
-                'Call of Battle = Goats - Gamesphere 4',
-                69.99),
-        new Item('TJB84HAA8OA',
-                'Bricko Building Blocks',
-                4.99));
+    new Item(
+        'WYK12EPU5EZ',
+        'Call of Battle = Goats - Gamesphere 4',
+        69.99
+    ),
+    new Item(
+        'TJB84HAA8OA',
+        'Bricko Building Blocks',
+        4.99
+    )
+);
 $order3->total = 74.98;
 $order3->orderDate = '2013-11-03 17:45:28';
 $orders[] = $order3;
@@ -140,16 +163,22 @@ $orderSummariesBucket = $client->bucket('OrderSummaries');
 
 
 // Storing Data
-$customer_riak = $customersBucket->newObject(strval($customer->customerId), $customer);
+$customer_riak = $customersBucket->newObject(
+    strval($customer->customerId), $customer
+);
 $customer_riak->store();
 
 foreach ($orders as $order) {
-    $order_riak = $ordersBucket->newObject(strval($order->orderId), $order);
+    $order_riak = $ordersBucket->newObject(
+        strval($order->orderId), $order
+    );
     $order_riak->store();
 }
 unset($order);
 
-$order_summary_riak = $orderSummariesBucket->newObject(strval($orderSummary->customerId), $orderSummary);
+$order_summary_riak = $orderSummariesBucket->newObject(
+    strval($orderSummary->customerId), $orderSummary
+);
 $order_summary_riak->store();
 
 
@@ -163,12 +192,12 @@ print_r($fetched_customer);
 
 // Adding Index Data
 $keys = array(1,2,3);
-foreach($keys as $key) {
+foreach ($keys as $key) {
     $order = $ordersBucket->get(strval($key));
     $salespersonId = $order->data['salespersonId'];
     $orderDate = $order->data['orderDate'];
-    $order->addIndex('SalespersonId','int',$salespersonId);
-    $order->addIndex('OrderDate','bin',$orderDate);
+    $order->addIndex('SalespersonId', 'int', $salespersonId);
+    $order->addIndex('OrderDate', 'bin', $orderDate);
     $order->store();
 }
 unset($key);
@@ -181,8 +210,11 @@ $janes_orders = $ordersBucket->indexSearch('SalespersonId', 'int', 9000);
 print("\n\nJane's Orders: \n");
 print_r($janes_orders);
 
-// Query for orders where the OrderDate bin index is between 2013-10-01 and 2013-10-31
-$october_orders = $ordersBucket->indexSearch('OrderDate', 'bin', '2013-10-01', '2013-10-31');
+// Query for orders where the OrderDate bin index is 
+// between 2013-10-01 and 2013-10-31
+$october_orders = $ordersBucket->indexSearch(
+    'OrderDate', 'bin', '2013-10-01', '2013-10-31'
+);
 print("\n\nOctober's Orders: \n");
 print_r($october_orders);
 
